1b9e7d5f50d52afca130aa82c32f216f34d9741e
Pablo Souza <56857093+pablosouza20@users.noreply.github.com>
2022-10-20 01:14:06 -0300

    Delete EstruturaDeDecisao directory

--- EstruturaDeDecisao/ex02.py
+++
@@ -1,8 +0,0 @@
-'''
-2 - Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
-'''
-num = float(input('Digite qualquer valor sendo ele positivo ou negativo: '))
-if num >= 0:
-    print('O número {:.2f} é postivo!'.format(num))
-else:
-    print('O número {:.2f} é negativo!'.format(num))    
--- EstruturaDeDecisao/ex04.py
+++
@@ -1,9 +0,0 @@
-'''
-4 - Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
-'''
-letra = str(input('Digite alguma letra para verificarmos se é vogal ou consoante: ')).strip().upper()
-
-if letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U':
-  print('A letra {} é VOGAL!'.format(letra))
-else:
-  print('A letra {} é CONSOANTE!'.format(letra))  
